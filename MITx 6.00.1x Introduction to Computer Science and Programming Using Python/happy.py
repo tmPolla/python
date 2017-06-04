9fa63b208c4b863e00b648e59cc91b7004118318
Polla <marosi.polla@gmail.com>
2017-06-04 18:37:26 +0200

    Add files via upload

---
+++ MITx 6.00.1x Introduction to Computer Science and Programming Using Python/happy.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  4 13:29:32 2017
+
+@author: MarosiPA
+"""
+
+happy=2
+if (happy>2):
+    print("hello world")
+else:
+    print("happy")
+    
+    
+varA='a'
+varB=2
+if type(varA)=='str' or type(varB)=='str':
+    print("string involved")
+    
+    
---
+++ MITx 6.00.1x Introduction to Computer Science and Programming Using Python/varPrint.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  4 13:51:15 2017
+
+@author: MarosiPA
+"""
+
+varA='a'
+varB=2
+type
+if (type(varA)==str or type(varB)==str):
+    print("string involved")
+elif varA>varB:
+    print("bigger")
+elif varA==varB:
+    print("equal")
+else:
+    print("smaller")
