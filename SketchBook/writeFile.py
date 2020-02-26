89059d9f3fc1de474a144e5f5235f7150a5208b2
Polla <marosi.polla@gmail.com>
2020-02-26 11:40:27 +0100

    Add files via upload

---
+++ SketchBook/writeFile.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import os
+def create_python_script(filename):
+  comments = "# Start of a new Python program"
+  with open(filename, "w") as f:
+    f.write(comments)
+  
+  filesize = os.path.getsize(filename)
+  return(filesize)
+
+print(create_python_script("program.py"))
